df30cc2c9f54b7d2134b640082829efefa611932
clement picard zivy <clementpicardzivy@gmail.com>
2019-11-07 16:52:28 -0600

    UL done with test done

--- 04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,10 +1,9 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
-  return `<li class="list-group-item">${content}</li>`
+  return `  <li class="list-group-item">${content}</li>`
 };
 
-const liste = ['orange', 'baguette', 'cafe'];
-
+// OUT: ONE STRING
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
   lis = "";
@@ -14,6 +13,31 @@ const unorderedList = (items) => {
   return `<ul class="list-group">${lis}</ul>`
 };
 
-console.log(unorderedList(liste));
+
+// CORRECT LIST WITH INDENTATION
+const unorderedList2 = (items) => {
+  console.log('<ul class="list-group">');
+  items.map((item) => {
+    item = `  ${listItem(item)}`;
+    console.log(item);
+  });
+  console.log(`</ul>`)
+};
+
+const liste = ['orange', 'baguette', 'cafe'];
+console.log(unorderedList2(liste));
+
+
+// WITH MAP
+const unorderedListMap = array => array.map(el => listItem(el));
+const listGenerator = (array) => {
+  console.log('<ul class="list-group">');
+  console.log(unorderedListMap(array));
+  console.log('</ul>');
+};
+
+console.log(unorderedListMap(liste));
+
+
 
 module.exports = { listItem, unorderedList }; // Do not remove.
