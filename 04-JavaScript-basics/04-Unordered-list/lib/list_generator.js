76a32fb3f2cb9087faca0067c12760001302b31e
clement picard zivy <clementpicardzivy@gmail.com>
2019-11-07 16:16:10 -0600

    unordered list generator done rake OK

--- 04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
+++ 04-JavaScript-basics/04-Unordered-list/lib/list_generator.js
@@ -1,9 +1,19 @@
 const listItem = (content) => {
   // TODO: return the proper <li> HTML tag with its content (as a string)
+  return `<li class="list-group-item">${content}</li>`
 };
+
+const liste = ['orange', 'baguette', 'cafe'];
 
 const unorderedList = (items) => {
   // TODO: return the proper <ul> markup (as a string)
+  lis = "";
+  const generator = items.forEach((item) => {
+    lis += listItem(item);
+  });
+  return `<ul class="list-group">${lis}</ul>`
 };
+
+console.log(unorderedList(liste));
 
 module.exports = { listItem, unorderedList }; // Do not remove.
