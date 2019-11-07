b2f0163e18a667811c6755b60945aa0efb1c57f2
clement picard zivy <clementpicardzivy@gmail.com>
2019-11-07 17:16:27 -0600

    isGood check ok

--- 04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -1,7 +1,8 @@
 const isGood = (soldierType) => {
-  // TODO: return `true` if the soldierType is Good.
-  // Hint: Hobbits, Elves, Dwarves and Eagles are Good
+  const goodGuys = ['hobbits', 'elves', 'dwarves', 'eagles'];
+  return goodGuys.includes(soldierType.toLowerCase()) ? true : false;
 };
+
 
 const buildSoldierObject = (battlefield) => {
   // TODO: Given a battlefield (String), return an object of forces.
@@ -10,5 +11,7 @@ const buildSoldierObject = (battlefield) => {
 const whoWinsTheWar = (battlefield) => {
   // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
 };
+
+
 
 module.exports = { whoWinsTheWar, buildSoldierObject, isGood }; // Do not remove this line
