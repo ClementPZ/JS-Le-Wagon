e7f028e54c748ef151b79a998cda9ba91bdbadf8
clement picard zivy <clementpicardzivy@gmail.com>
2019-11-07 13:50:02 -0600

    Valid mail done rake OK

--- 04-JavaScript-basics/03-Valid-email/lib/valid_email.js
+++ 04-JavaScript-basics/03-Valid-email/lib/valid_email.js
@@ -1,5 +1,11 @@
 const valid = (email) => {
   // TODO: return true if the `email` string has the right pattern!
+  cadena = email;
+  expresion = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const check = cadena.match(expresion) ? true : false;
+  return check;
 };
+
+console.log(valid("boris@paillard.dm"));
 
 module.exports = valid; // Do not remove.
