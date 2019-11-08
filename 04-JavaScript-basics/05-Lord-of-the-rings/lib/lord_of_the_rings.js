0ca6205fddaba0d5c470116bb7d20a7de28904c7
clement picard zivy <clementpicardzivy@gmail.com>
2019-11-07 18:39:46 -0600

    buildObject draft ugly done

--- 04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 04-JavaScript-basics/05-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -3,15 +3,22 @@ const isGood = (soldierType) => {
   return goodGuys.includes(soldierType.toLowerCase()) ? true : false;
 };
 
-
 const buildSoldierObject = (battlefield) => {
-  // TODO: Given a battlefield (String), return an object of forces.
+  let team = {};
+  const battlefieldArray = battlefield.split(',');
+  battlefieldArray.forEach((element) => {
+    const soldierType = element.split(':');
+    console.log(soldierType[0]);
+    team[soldierType[0]] = Number.parseInt(soldierType[1], 10);
+  });
+  return team
 };
+
+const goodGuys = 'Hobbits:4,Dwarves:1,Elves:1,Goblins:100,UrukHai:1';
+console.log(buildSoldierObject(goodGuys));
 
 const whoWinsTheWar = (battlefield) => {
-  // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
+  // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie"
 };
-
-
 
 module.exports = { whoWinsTheWar, buildSoldierObject, isGood }; // Do not remove this line
